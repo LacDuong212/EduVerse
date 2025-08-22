dbe3d8b7b588ca5efd8a99634875a6e3dd8e57b3
LacDuong <lacduongldg212@gmail.com>
2025-08-22 23:21:22 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/mongodb.js';
+
+//Initialize Express
+const app = express();
+
+// Connect to MongoDB
+await connectDB();
+
+//Middleware
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => res.send("API is running"));
+
+// Port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
