cd36c2661f7a2ad15f9ba285097c9fd5489604f7
Vi021 <148944804+Vi021@users.noreply.github.com>
2025-08-25 21:10:42 +0700

    User Profile frontend + shadnc ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import EmailVerify from './pages/EmailVerify'
+import Profile from './pages/Profile'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   )
