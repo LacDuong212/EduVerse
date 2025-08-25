b8e13e69d67ebfc153e81a73d44871084975f7f1
Vi021 <148944804+Vi021@users.noreply.github.com>
2025-08-25 15:35:51 +0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,19 +13,21 @@ const app = express();
 // Connect to MongoDB
 await connectDB();
 
+const allowedOrigins = ['http://localhost:5173'];
+
 //Middleware
-app.use(cors({credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}));
 
 // API Endpoints
 app.get('/', (req, res) => res.send("API is running"));
 app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter);
+app.use('/api/user', userRouter);
 
 // Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
