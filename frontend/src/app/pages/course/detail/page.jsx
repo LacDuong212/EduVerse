0676edc478231f92cf54661c060120d886486c72
Vi021 <148944804+Vi021@users.noreply.github.com>
2025-11-02 20:55:47 +0700

    Course Detail bug: extra topbar & footer

--- frontend/src/app/pages/course/detail/page.jsx
+++ frontend/src/app/pages/course/detail/page.jsx
@@ -3,8 +3,6 @@ import CourseDetails from './components/CourseDetails';
 import ListedCourses from './components/ListedCourses';
 import PageIntro from './components/PageIntro';
 import useCourseDetail from './useCourseDetail';
-import TopNavigationBar from '@/components/StudentLayoutComponents/TopNavigationBar';
-import Footer from '@/components/Footer';
 const CourseDetail = () => {
   const { course, loading, error, refetch } = useCourseDetail();
 
@@ -23,13 +21,11 @@ const CourseDetail = () => {
 
   return <>
       <PageMetaData title="Course Detail" />
-      <TopNavigationBar />
       <main>
         <PageIntro course={course} />
         <CourseDetails course={course} />
         <ListedCourses  />
       </main>
-      <Footer className="bg-light" />
     </>;
 };
 export default CourseDetail;
