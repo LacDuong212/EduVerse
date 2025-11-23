33dd6f1ddf1bb7cdc1d5035b1501340aaca94d40
LacDuong <lacduongldg212@gmail.com>
2025-11-23 23:13:34 +0700

    Fix UI

--- frontend/src/app/pages/course/grid/components/Banner.jsx
+++ frontend/src/app/pages/course/grid/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
     <Container>
       <Row>
         <Col xs={12} className="text-center">
-          <h1 className="m-0">All Course</h1>
+          <h1 className="m-0 text-light">All Course</h1>
         </Col>
       </Row>
     </Container>
