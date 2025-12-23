8bafd1ff5f9f2723005f69a535ae7b797c0e286c
LacDuong <lacduongldg212@gmail.com>
2025-12-23 20:02:17 +0700

    Add "User-Agent" for MOMO

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -76,7 +76,8 @@ const createMomoPaymentRequest = (orderId, amount, orderInfo, requestId, extraDa
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(requestBody)
+                'Content-Length': Buffer.byteLength(requestBody),
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
             }
         };
 
