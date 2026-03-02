387330bb6d958dc153691284430f1b89b21ad353
Vi021 <hellno83737@gmail.com>
2026-03-02 15:58:26 +0700

    move configs files

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,12 @@
+import { v2 as cloudinary } from "cloudinary";
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
---
+++ backend/config/socket.js
@@ -0,0 +1,54 @@
+import { Server } from "socket.io";
+
+let io;
+let onlineUsers = [];
+
+export const initSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: [
+        "http://localhost:5173", 
+        "http://localhost:5174",
+        process.env.CLIENT_URL,
+      ],
+      methods: ["GET", "POST"],
+      credentials: true
+    },
+    transports: ['polling', 'websocket'],
+    pingTimeout: 60000, 
+    
+    connectionStateRecovery: {
+      maxDisconnectionDuration: 2 * 60 * 1000,
+      skipMiddlewares: true,
+    }
+  });
+
+  io.on("connection", (socket) => {
+    // 1. Khi user login, báo danh
+    socket.on("newUser", (userId) => {
+      addNewUser(userId, socket.id);
+    });
+
+    // 2. Khi user tắt tab
+    socket.on("disconnect", () => {
+      removeUser(socket.id);
+    });
+  });
+
+  return io;
+};
+
+// --- Helper Functions ---
+const addNewUser = (userId, socketId) => {
+  // Xóa socket cũ của user đó nếu có (tránh duplicate)
+  onlineUsers = onlineUsers.filter(u => u.userId !== userId); 
+  onlineUsers.push({ userId, socketId });
+  console.log("Online Users:", onlineUsers);
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+export const getIO = () => io;
+export const getOnlineUsers = () => onlineUsers;
