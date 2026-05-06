f8c2598a4b260b2dae9b14fa4fd2a99ff91a785f
Vi021 <hellno83737@gmail.com>
2026-05-06 21:15:56 +0700

    what's done: - refactored chatbot component

--- backend/src/modules/chatbot/chatbot.validation.js
+++ backend/src/modules/chatbot/chatbot.validation.js
@@ -3,7 +3,13 @@ import { z } from "zod";
 export const chatbotRequest = z.object({
   body: z.object({
     sessionId: z.string("Session ID is required").optional(),
-    message: z.string("Message is required"),
-    language: z.enum(["en", "vi"]).optional().default("en")
+    message: z.string("Message is required").min(1, "Message cannot be empty"),
+    language: z.preprocess(
+      (val) => {
+        if (typeof val === "string") return val.toLowerCase();
+        return val;
+      },
+      z.enum(["en", "vi"],).optional().default("en")
+    ),
   })
 });
