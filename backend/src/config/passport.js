f4749ed086deb3b62169c1425a216bd7c5e78d14
LacDuong <lacduongldg212@gmail.com>
2026-05-21 21:56:41 +0700

    #EDV-281: Fix new user login by Google

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,6 +1,7 @@
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import userModel from "#modules/user/user.model.js";
 import { toAuthUserDto } from "#modules/user/user.mapper.js";
+import { createNewStudent } from "#modules/student/student.service.js";
 
 export default function(passport) {
   passport.use(
@@ -12,36 +13,49 @@ export default function(passport) {
         proxy: true,
       },
       async (accessToken, refreshToken, profile, done) => {
-        const email = profile.emails[0].value;
-        const name = profile.displayName;
-        const googleId = profile.id;
-        const pfpImg = profile.photos[0].value;
-
         try {
-          let user = await userModel.findOne({ googleId: googleId });
+          const email = profile.emails?.[0]?.value?.toLowerCase();
+          const name = profile.displayName;
+          const googleId = profile.id;
+          const pfpImg = profile.photos?.[0]?.value;
+
+          if (!email) {
+            return done(null, false, { message: "Google account has no email." });
+          }
+
+          let user = await userModel.findOne({ googleId });
+
           if (user) {
             return done(null, toAuthUserDto(user));
           }
 
-          user = await userModel.findOne({ email: email });
+          user = await userModel.findOne({ email });
+
           if (user) {
             user.googleId = googleId;
+
             if (!user.pfpImg) {
               user.pfpImg = pfpImg;
             }
+
+            user.isVerified = true;
+
             await user.save();
+
             return done(null, toAuthUserDto(user));
           }
 
           const newUser = await userModel.create({
-            googleId: googleId,
-            name: name,
-            email: email,
-            pfpImg: pfpImg,
+            googleId,
+            name,
+            email,
+            pfpImg,
             isVerified: true,
           });
-          return done(null, toAuthUserDto(newUser));
 
+          await createNewStudent(newUser._id);
+
+          return done(null, toAuthUserDto(newUser));
         } catch (err) {
           return done(err, false);
         }
